4a0ed12dd7e560dede6070cd06b5e5106742626d
gaurav-kushwah <gkkushwah523@gmail.com>
2024-10-02 14:57:26 +0530

    "Exploring get post api methods"

--- homework.md
+++ homework.md
@@ -1,11 +1,28 @@
- - create a repo=sitry
- - Initalize the repo=sitry
+ <-----------------------> Video 3 homework ---> 
+ - create a repositry
+ - Initalize the repositry
  - node modulues, package.json, package-lock.json
- - Install Eapress
+ - Install Express Lbrary
  - create a server
- - Li=sten to port 7777
+ - Listen to port 7777
  - Write  request handler for /test , /hello
- - in=stall nodemon and update the scripts
- -what are dependencies
- - ue of -g command
+ - install nodemon and update the scripts
+ - what are dependencies
+ - use of -g command
  - difference between caret anf tilde
+
+
+
+
+
+ Homework video 4 -: 
+
+ -play with route and route handlers extensions - ./hello, /test,/hello2,/,/xyz
+
+ tip ----> order of routes matter a lot
+
+
+ - Install Postman app and make a workspace/collections  ==> test api calls 
+
+
+ - Write logic to handle Get, Post, Patch, Delete API calls and test them on Postman 
--- src/app.js
+++ src/app.js
@@ -9,6 +9,51 @@ app.listen(port,()=>{
 
 })
 
-app.use((req,res)=>{
+
+app.get("/user",(req,res)=>{
+    res.send({"name":Akshat,"surname":Bharucha});
+})
+
+
+app.post("/user",(req,res)=>{
+    res.send("User Created Successfully");
+})
+
+
+
+app.put("/user",(req,res)=>{
+    res.send("User updated Successfully");
+})
+
+
+app.delete("/user",(req,res)=>{
+    res.send("User deleted Successfully");
+})
+
+
+
+
+// app.use("/hello/1",(req,res)=>{
+//     res.send("server is started on ............./hello/1.............")
+// })
+// app.use("/hello/2",(req,res)=>{
+//     res.send("server is started on ............./hello/2.............")
+// })
+
+// app.use("/hello",(req,res)=>{
+//     res.send("server is started on ............./hello.............")
+// })
+
+// app.use("/test",(req,res)=>{
+//     res.send("server is started on ............./test.............")
+// })
+
+app.use("",(req,res)=>{
     res.send("Hey you got response from server");
+})  
+
+ // "" and "/" is the same case when it comes to extension
+ 
+app.use("/",(req,res)=>{
+    res.send("server is started on ............./.............")
 })
